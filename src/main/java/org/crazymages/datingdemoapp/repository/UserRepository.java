a61965101869f153d46556af1bfe093d6636b42b
andyBzg <andr.bzg@gmail.com>
2023-05-18 17:16:57 +0200

    commit

--- src/main/java/org/crazymages/datingdemoapp/repository/UserRepository.java
+++ src/main/java/org/crazymages/datingdemoapp/repository/UserRepository.java
@@ -4,9 +4,28 @@ import org.crazymages.datingdemoapp.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Collection;
+
 @Repository
 public interface UserRepository extends JpaRepository<User, Integer> {
 
     void deleteUserByName(String name);
 
+    User getUserById(Integer id);
+
+    User getUserByName(String name);
+
+    /*Collection<User> getUsersByIdAfterOrderByNameDesc(int id);
+    //Найти всех пользователей, id которых "после" int id
+    // и отсортировать их по имени в обратном порядке
+
+    Collection<User> getUsersByIdIsIn(Collection<Integer> id);
+    //SELECT * FROM users WHERE id IN (....)
+
+    User getTopById(int id);
+
+    void deleteUserByNameEndsWith(String endsWith);
+    //DELETE FROM users WHERE name ilake '%.....'
+
+    void updateUserById(Integer id);*/
 }
