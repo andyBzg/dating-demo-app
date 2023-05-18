60ae0f5be0c2c1114e858b170c64b5f7270737d3
andyBzg <andr.bzg@gmail.com>
2023-05-18 17:16:27 +0200

    commit

--- src/main/java/org/crazymages/datingdemoapp/controller/DatingAppController.java
+++ src/main/java/org/crazymages/datingdemoapp/controller/DatingAppController.java
@@ -1,12 +1,17 @@
 package org.crazymages.datingdemoapp.controller;
 
-import jakarta.transaction.Transactional;
+import lombok.extern.slf4j.Slf4j;
 import org.crazymages.datingdemoapp.entity.User;
 import org.crazymages.datingdemoapp.service.database.UsersDatabaseService;
 import org.crazymages.datingdemoapp.service.match.MatchService;
 import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
+
+@Slf4j
 @RestController
 public class DatingAppController {
 
@@ -21,20 +26,39 @@ public class DatingAppController {
     }
 
 
-    @GetMapping(value = "/new-match")
-    public User getNewMatch() {
-        return matchService.getNewMatch();
-    }
-
     @PostMapping(value = "/user/add")
     public void insertUser(@RequestBody User user) {
         usersDatabaseService.add(user);
     }
 
-    @Transactional
+    @GetMapping(value = "/new-match")
+    public User getNewMatch() {
+        return matchService.getNewMatch();
+    }
+
+    @GetMapping(value = "/user/get-all")
+    public List<User> getAllUsers() {
+        return usersDatabaseService.getUsersList();
+    }
+
+    @GetMapping(value = "/user/{id}")
+    public User getUser(@PathVariable(name = "id") Integer id) throws InterruptedException {
+        return usersDatabaseService.getUserById(id);
+    }
+
+    @PutMapping(value = "/user/update/{id}")
+    public void updateUser(@PathVariable Integer id, @RequestBody User user) {
+        usersDatabaseService.updateUser(id, user);
+    }
+
     @DeleteMapping(value = "/user/delete/by-name/{name}")
     public void deleteByName(@PathVariable String name) {
         usersDatabaseService.deleteByName(name);
+    }
+
+    @PutMapping(value = "/gift/{from}/{to}")
+    public void gift(@PathVariable(value = "from") Integer fromId, @PathVariable(value = "to") Integer toId) {
+        usersDatabaseService.transferPoints(fromId, toId);
     }
 
 }
