082bfa0a9724b7b488cb609b749dfc24c3944c42
andyBzg <andr.bzg@gmail.com>
2023-05-27 13:50:47 +0200

    add logging to match service methods

--- src/main/java/org/crazymages/datingdemoapp/service/match/impl/HighestRatingMatchService.java
+++ src/main/java/org/crazymages/datingdemoapp/service/match/impl/HighestRatingMatchService.java
@@ -1,12 +1,15 @@
 package org.crazymages.datingdemoapp.service.match.impl;
 
+import lombok.extern.slf4j.Slf4j;
 import org.crazymages.datingdemoapp.entity.User;
 import org.crazymages.datingdemoapp.service.database.UsersDatabaseService;
 import org.crazymages.datingdemoapp.service.match.MatchService;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
+@Slf4j
 @Service
 //@Primary
 public class HighestRatingMatchService implements MatchService {
@@ -18,6 +21,7 @@ public class HighestRatingMatchService implements MatchService {
     }
 
     @Override
+    @Transactional
     public User getNewMatch() {
         List<User> users = usersDatabaseService.getUsersList();
         int maxRating = 0;
@@ -29,7 +33,7 @@ public class HighestRatingMatchService implements MatchService {
                 highestRatedUser = user;
             }
         }
-
+        log.info("Found new match with user: {}", highestRatedUser);
         return highestRatedUser;
     }
 }
--- src/main/java/org/crazymages/datingdemoapp/service/match/impl/RandomMatchService.java
+++ src/main/java/org/crazymages/datingdemoapp/service/match/impl/RandomMatchService.java
@@ -1,14 +1,17 @@
 package org.crazymages.datingdemoapp.service.match.impl;
 
+import lombok.extern.slf4j.Slf4j;
 import org.crazymages.datingdemoapp.entity.User;
 import org.crazymages.datingdemoapp.service.database.UsersDatabaseService;
 import org.crazymages.datingdemoapp.service.match.MatchService;
 import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Random;
 
+@Slf4j
 @Service
 @Primary
 public class RandomMatchService implements MatchService {
@@ -20,10 +23,12 @@ public class RandomMatchService implements MatchService {
     }
 
     @Override
+    @Transactional
     public User getNewMatch() {
         List<User> users = usersDatabaseService.getUsersList();
         Random random = new Random();
         int i = random.nextInt(users.size());
+        log.info("Found new match with user: id={}", i);
         return users.get(i);
     }
 }
